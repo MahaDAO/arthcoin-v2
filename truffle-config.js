3aa5ae2ba81247a7569a7d452d462e4e2400f6c7
Yash Agrawal <yashagrawal18@outlook.com>
2021-03-23 22:15:42 +0530

    Update solc version in truffle config

--- truffle-config.js
+++ truffle-config.js
@@ -46,7 +46,7 @@ module.exports = {
   },
   compilers: {
     solc: {
-      version: '0.8.0+commit.c7dfd78e'
+      version: '0.8.0+commit.c7dfd78e',
       settings: {
         optimizer: {
           enabled: true,
