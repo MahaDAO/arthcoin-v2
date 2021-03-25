b64c4b4bd4fb091a29eb48860faad5dd7cd2fa14
Yash Agrawal <yashagrawal18@outlook.com>
2021-03-25 21:14:52 +0530

    Change Chainlink oracle to use GMU price rather than USD

--- contracts/Oracle/ChainlinkETHUSDPriceConsumer.sol
+++ contracts/Oracle/ChainlinkETHUSDPriceConsumer.sol
@@ -3,23 +3,51 @@
 pragma solidity ^0.8.0;
 pragma experimental ABIEncoderV2;
 
-import "./AggregatorV3Interface.sol";
+import './ISimpleOracle.sol';
+import '../Math/SafeMath.sol';
+import './AggregatorV3Interface.sol';
 
 contract ChainlinkETHUSDPriceConsumer {
+    using SafeMath for uint256;
+
+    /**
+     * State variables.
+     */
+
+    ISimpleOracle public gmuOracle;
     AggregatorV3Interface internal priceFeed;
 
-    constructor() {
+    uint256 public priceFeedDecimals = 8;
+
+    constructor(ISimpleOracle gmuOracle_) {
         priceFeed = AggregatorV3Interface(
             0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419
         );
+
+        gmuOracle = gmuOracle_;
+        priceFeedDecimals = priceFeed.decimals();
     }
 
     /**
      * Returns the latest price
      */
-    function getLatestPrice() public view returns (int256) {
+
+    function getGmuPrice() public view returns (uint256) {
+        // Considering that gmuOracle uses 1e18 as precision.
+
+        uint256 decimalsDiff = uint256(18).sub(priceFeedDecimals);
+
+        return gmuOracle.getPrice().div(10**decimalsDiff);
+    }
+
+    function getLatestUSDPrice() public view returns (uint256) {
         (, int256 price, , , ) = priceFeed.latestRoundData();
-        return price;
+
+        return uint256(price);
+    }
+
+    function getLatestPrice() public view returns (uint256) {
+        return getLatestUSDPrice().mul(getGmuPrice()).div(priceFeedDecimals);
     }
 
     function getDecimals() public view returns (uint8) {
