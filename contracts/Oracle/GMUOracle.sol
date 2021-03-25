ca184f9d70385866998944353b2e128b11c14041
Yash Agrawal <yashagrawal18@outlook.com>
2021-03-25 17:53:04 +0530

    Fix simple oracle and GMU oracle

--- contracts/Oracle/GMUOracle.sol
+++ contracts/Oracle/GMUOracle.sol
@@ -2,29 +2,10 @@
 
 pragma solidity ^0.8.0;
 
-import {SafeMath} from '@openzeppelin/contracts/contracts/math/SafeMath.sol';
-import '@openzeppelin/contracts/contracts/access/Ownable.sol';
+import './SimpleOracle.sol';
 
-import './ISimpleOracle.sol';
-
-contract GMUOracale is Ownable {
-    using SafeMath for uint256;
-
-    uint256 public gmu = 1e18;
-
-    constructor(uint256 _gmu) {
-        gmu = _gmu;
-    }
-
-    function setGMU(uint256 _gmu) public onlyOwner {
-        require(_gmu >= 0, 'Oracle: price cannot be < 0');
-
-        gmu = _gmu;
-    }
-
-    function getGMU() public view returns (uint256) {
-        return gmu;
-    }
-
-    event Updated(uint256 GMU);
+contract GMUOracale is SimpleOracle {
+    constructor(string memory _name, uint256 _price)
+        SimpleOracle(_name, _price)
+    {}
 }
--- contracts/Oracle/SimpleOracle.sol
+++ contracts/Oracle/SimpleOracle.sol
@@ -2,33 +2,40 @@
 
 pragma solidity ^0.8.0;
 
-import {SafeMath} from '@openzeppelin/contracts/contracts/math/SafeMath.sol';
 import '@openzeppelin/contracts/contracts/access/Ownable.sol';
 
 import './ISimpleOracle.sol';
 
 contract SimpleOracle is Ownable, ISimpleOracle {
-    using SafeMath for uint256;
-
+    /**
+     * State variables.
+     */
     string public name;
     uint256 public price = 1e18;
 
+    /**
+     * Constructor.
+     */
     constructor(string memory _name, uint256 _price) {
         name = _name;
 
-        // Set the initial price to 1e18 i.e 1.
+        // Set the initial price.
         price = _price;
     }
 
+    /**
+     * Views.
+     */
+    function getPrice() public view override returns (uint256) {
+        return price;
+    }
+
+    /**
+     * Settes.
+     */
     function setPrice(uint256 _price) public onlyOwner {
         require(_price >= 0, 'Oracle: price cannot be < 0');
 
         price = _price;
     }
-
-    function getPrice() public view override returns (uint256) {
-        return price;
-    }
-
-    event Updated(uint256 price0CumulativeLast, uint256 price1CumulativeLast);
 }
