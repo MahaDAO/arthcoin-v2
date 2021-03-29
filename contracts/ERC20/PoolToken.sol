9e8c0655ddd1f57df01620752f6617629d655fc9
Steven Enamakel <enamakel@vezures.com>
2021-03-29 10:01:39 +0300

    mionr changes

--- contracts/ERC20/PoolToken.sol
+++ contracts/ERC20/PoolToken.sol
@@ -2,18 +2,14 @@
 
 pragma solidity ^0.8.0;
 
-import '@openzeppelin/contracts/contracts/access/AccessControl.sol';
-import '@openzeppelin/contracts/contracts/token/ERC20/SafeERC20.sol';
-import {Ownable} from '@openzeppelin/contracts/contracts/access/Ownable.sol';
-import {SafeMath} from '@openzeppelin/contracts/contracts/math/SafeMath.sol';
-import {ERC20} from '@openzeppelin/contracts/contracts/token/ERC20/ERC20.sol';
-import {IERC20} from '@openzeppelin/contracts/contracts/token/ERC20/IERC20.sol';
-
+import '../Governance/AccessControl.sol';
+import '../Math/SafeMath.sol';
+import {ERC20} from './ERC20.sol';
+import {IERC20} from './IERC20.sol';
 import {Math} from '../Math/Math.sol';
 
 contract PoolToken is AccessControl, ERC20 {
     using SafeMath for uint256;
-    using SafeERC20 for IERC20;
 
     /**
      * State variables.
@@ -30,7 +26,7 @@ contract PoolToken is AccessControl, ERC20 {
     modifier onlyAdmin {
         require(
             hasRole(DEFAULT_ADMIN_ROLE, _msgSender()),
-            'Lending: FORBIDDEN'
+            'PoolToken: FORBIDDEN'
         );
         _;
     }
@@ -51,11 +47,10 @@ contract PoolToken is AccessControl, ERC20 {
         IERC20[] memory poolTokens_
     ) ERC20(tokenName, tokenSymbol) {
         poolTokens = poolTokens_;
-
         _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
     }
 
-    function setPoolToken(IERC20 token) public onlyAdmin {
+    function addPoolToken(IERC20 token) public onlyAdmin {
         poolTokens.push(token);
     }
 
@@ -63,61 +58,21 @@ contract PoolToken is AccessControl, ERC20 {
     //     enableDeposits = val;
     // }
 
-    function _withdraw(uint256 liquidity) internal {
-        require(liquidity > 0, 'Lending: amount = 0');
+    function _withdraw(uint256 amount) internal {
+        require(amount > 0, 'Lending: amount = 0');
+
+        // calculate how much share of the supply the user has
+        uint256 percentage = amount.mul(1e8).div(totalSupply());
 
         for (uint256 i = 0; i < poolTokens.length; i++) {
             uint256 balance = poolTokens[i].balanceOf(address(this));
-            // https://github.com/Uniswap/uniswap-v2-core/blob/master/contracts/UniswapV2Pair.sol#L134
-            uint256 amount = liquidity.mul(balance).div(totalSupply());
-
-            if (amount > 0) poolTokens[i].safeTransfer(msg.sender, amount);
+            uint256 shareAmount = balance.mul(percentage).div(1e8);
+            if (shareAmount > 0)
+                poolTokens[i].safeTransfer(msg.sender, shareAmount);
         }
 
-        _burn(msg.sender, liquidity);
+        _burn(msg.sender, amount);
 
-        emit Withdraw(msg.sender, liquidity);
+        emit Withdraw(msg.sender, amount);
     }
-
-    // // NOTE: need to check, test and confirm this piece of code.
-    // function _deposit(address[] memory tokens, uint256[] memory amounts)
-    //     internal
-    // {
-    //     require(enableDeposits, 'PoolToken: deposits disabled');
-    //     require(tokens.length == amounts.length, 'PoolToken: invalid params');
-    //     require(tokens.length == poolTokens.length, 'PoolToken: invalid param');
-
-    //     uint256 amountToMint = 0;
-    //     uint256 poolTotalSupply = totalSupply();
-
-    //     for (uint256 i = 0; i < tokens.length; i++) {
-    //         address token = tokens[i];
-    //         uint256 amount = amounts[i];
-    //
-    //          // TODO: add a way to check that `token` is in poolTokens array(or is supported for this pool).
-    //         require(amount > 0, 'PoolToken: amount = 0');
-    //
-    //         https://github.com/Uniswap/uniswap-v2-core/blob/master/contracts/UniswapV2Pair.sol#L118
-    //         if (poolTotalSupply == 0)
-    //             // Confused in this part.
-    //             amountToMint = amountToMint.mul(Math.sqrt(amount));
-    //         else {
-    //             uint256 balance = IERC20(token).balanceOf(address(this));
-    //             uint256 liquidity = amount.mul(poolTotalSupply).div(balance);
-
-    //             if (i == 0) amountToMint = liquidity;
-    //             else amountToMint = Math.min(amountToMint, liquidity);
-    //         }
-
-    //         IERC20(token).safeTransferFrom(msg.sender, address(this), amount);
-    //     }
-
-    //     if (poolTotalSupply == 0) {
-    //         amountToMint = amountToMint.sub(MINIMUM_LIQUIDITY);
-    //         _mint(address(0), MINIMUM_LIQUIDITY);
-    //     }
-
-    //     _mint(msg.sender, amountToMint);
-    //     emit Deposit(msg.sender, amountToMint);
-    // }
 }
--- contracts/Oracle/SimpleOracle.sol
+++ contracts/Oracle/SimpleOracle.sol
@@ -2,40 +2,27 @@
 
 pragma solidity ^0.8.0;
 
-import '@openzeppelin/contracts/contracts/access/Ownable.sol';
-
+import '../Common/Ownable.sol';
 import './ISimpleOracle.sol';
 
 contract SimpleOracle is Ownable, ISimpleOracle {
-    /**
-     * State variables.
-     */
     string public name;
     uint256 public price = 1e18;
 
-    /**
-     * Constructor.
-     */
     constructor(string memory _name, uint256 _price) {
         name = _name;
-
-        // Set the initial price.
         price = _price;
     }
 
-    /**
-     * Views.
-     */
     function getPrice() public view override returns (uint256) {
         return price;
     }
 
-    /**
-     * Settes.
-     */
     function setPrice(uint256 _price) public onlyOwner {
         require(_price >= 0, 'Oracle: price cannot be < 0');
-
         price = _price;
+        emit PriceChange(block.timestamp, _price);
     }
+
+    event PriceChange(uint256 timestamp, uint256 price);
 }
