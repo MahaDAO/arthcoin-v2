6bf68f02dcb38508d45e9f20cbe213a38002217a
Sagar Behara <sagar@cryptocontrol.io>
2021-04-02 20:44:05 +0530

    fei's _transfer function

---
+++ contracts/Arth/IncentiveController.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+pragma experimental ABIEncoderV2;
+
+contract IncentiveController {
+    function incentivize(
+        address sender,
+        address recipient,
+        address,
+        uint256 amount
+    ) public {}
+}
