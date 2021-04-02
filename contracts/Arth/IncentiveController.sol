2ae5184e7a7aa05db295c3d0dd467994f39273d3
Sagar Behara <sagar@cryptocontrol.io>
2021-04-02 20:53:22 +0530

    Incentives setup

--- contracts/Arth/IncentiveController.sol
+++ contracts/Arth/IncentiveController.sol
@@ -3,11 +3,39 @@
 pragma solidity ^0.8.0;
 pragma experimental ABIEncoderV2;
 
-contract IncentiveController {
+import '../Math/SafeMath.sol';
+import '../Governance/AccessControl.sol';
+
+//import '../'
+
+contract IncentiveController is AccessControl {
+    modifier onlyAdmin() {
+        require(
+            hasRole(DEFAULT_ADMIN_ROLE, _msgSender()),
+            'You are not the owner or the governance timelock'
+        );
+        _;
+    }
+
+    constructor() public {
+        _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
+    }
+
     function incentivize(
         address sender,
         address recipient,
         address,
         uint256 amount
-    ) public {}
+    ) public {
+        require(sender != receiver, 'UniswapIncentive: cannot send self');
+        updateOracle();
+
+        if (_isPair(sender)) {
+            _incentivizeBuy(receiver, amountIn);
+        }
+
+        if (_isPair(receiver)) {
+            _incentivizeSell(sender, amountIn);
+        }
+    }
 }
