92928badb810c97c69f3e3602cd4861e94fc9930
Yash Agrawal <yashagrawal18@outlook.com>
2021-04-08 01:15:48 +0530

    Clean code in timelock

--- contracts/Governance/AccessControl.sol
+++ contracts/Governance/AccessControl.sol
@@ -2,9 +2,9 @@
 
 pragma solidity ^0.8.0;
 
-import '../Utils/EnumerableSet.sol';
 import '../Utils/Address.sol';
 import '../Common/Context.sol';
+import '../Utils/EnumerableSet.sol';
 
 /**
  * @dev Contract module that allows children to implement role-based access
