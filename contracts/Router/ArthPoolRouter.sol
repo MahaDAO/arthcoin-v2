c17b14f6f5fe6ae07caa9ce588e6fb6feb2e0bb6
Steven Enamakel <enamakel@vezures.com>
2021-04-10 00:13:13 +0300

    added WETH into arth pool router

--- contracts/Router/ArthPoolRouter.sol
+++ contracts/Router/ArthPoolRouter.sol
@@ -6,16 +6,23 @@ import {IARTH} from '../Arth/IARTH.sol';
 import {IARTHPool} from '../Arth/Pools/IARTHPool.sol';
 import {IARTHX} from '../ARTHX/IARTHX.sol';
 import {IERC20} from '../ERC20/IERC20.sol';
+import {IWETH} from '../ERC20/IWETH.sol';
 import {ISimpleOracle} from '../Oracle/ISimpleOracle.sol';
 import {IStakingRewards} from '../Staking/IStakingRewards.sol';
 
 contract ArthPoolRouter {
     IARTH private arth;
     IARTHX private arthx;
+    IWETH private weth;
 
-    constructor(IARTHX _arthx, IARTH _arth) {
+    constructor(
+        IARTHX _arthx,
+        IARTH _arth,
+        IWETH _weth
+    ) {
         arth = _arth;
         arthx = _arthx;
+        weth = _weth;
     }
 
     function mint1t1ARTHAndStake(
@@ -44,7 +51,7 @@ contract ArthPoolRouter {
         uint256 arthOutMin,
         uint256 secs,
         IStakingRewards stakingPool
-    ) external {
+    ) public {
         arthx.transferFrom(msg.sender, address(this), arthxAmountD18);
 
         uint256 arthOut = pool.mintAlgorithmicARTH(arthxAmountD18, arthOutMin);
@@ -65,7 +72,7 @@ contract ArthPoolRouter {
         uint256 arthOutMin,
         uint256 secs,
         IStakingRewards stakingPool
-    ) external {
+    ) public {
         collateral.transferFrom(msg.sender, address(this), amount);
         arthx.transferFrom(msg.sender, address(this), arthxAmount);
 
@@ -84,13 +91,13 @@ contract ArthPoolRouter {
         IARTHPool pool,
         IERC20 collateral,
         uint256 amount,
-        uint256 ARTHXOutMin,
+        uint256 arthxOutMin,
         uint256 secs,
         IStakingRewards stakingPool
-    ) external {
+    ) public {
         collateral.transferFrom(msg.sender, address(this), amount);
 
-        uint256 arthxOut = pool.recollateralizeARTH(amount, ARTHXOutMin);
+        uint256 arthxOut = pool.recollateralizeARTH(amount, arthxOutMin);
         arthx.approve(address(stakingPool), uint256(arthxOut));
 
         if (address(stakingPool) != address(0)) {
@@ -98,5 +105,58 @@ contract ArthPoolRouter {
                 stakingPool.stakeLockedFor(msg.sender, arthxOut, secs);
             else stakingPool.stakeFor(msg.sender, arthxOut);
         }
+    }
+
+    function recollateralizeARTHAndStakeWithETH(
+        IARTHPool pool,
+        uint256 arthxOutMin,
+        uint256 secs,
+        IStakingRewards stakingPool
+    ) public payable {
+        weth.deposit{value: msg.value}();
+        recollateralizeARTHAndStake(
+            pool,
+            weth,
+            msg.value,
+            arthxOutMin,
+            secs,
+            stakingPool
+        );
+    }
+
+    function mint1t1ARTHAndStakeWithETH(
+        IARTHPool pool,
+        uint256 arthOutMin,
+        uint256 secs,
+        IStakingRewards stakingPool
+    ) public payable {
+        weth.deposit{value: msg.value}();
+        mint1t1ARTHAndStake(
+            pool,
+            weth,
+            msg.value,
+            arthOutMin,
+            secs,
+            stakingPool
+        );
+    }
+
+    function mintFractionalARTHAndStakeWithETH(
+        IARTHPool pool,
+        uint256 arthxAmount,
+        uint256 arthOutMin,
+        uint256 secs,
+        IStakingRewards stakingPool
+    ) public payable {
+        weth.deposit{value: msg.value}();
+        mintFractionalARTHAndStake(
+            pool,
+            weth,
+            msg.value,
+            arthxAmount,
+            arthOutMin,
+            secs,
+            stakingPool
+        );
     }
 }
