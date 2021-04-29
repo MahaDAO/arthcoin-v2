44edc2d20291b2e3771e97ccde0b61ba359226c0
Steven Enamakel <enamakel@vezures.com>
2021-04-29 21:22:18 +0300

    update cr fn

--- scripts/setCRs.ts
+++ scripts/setCRs.ts
@@ -2,33 +2,22 @@ import { network, ethers } from 'hardhat';
 
 require('dotenv').config();
 
-const oracles = [
-  'UniswapPairOracle_ARTH_USDC',
-  'UniswapPairOracle_ARTH_USDT',
-  'UniswapPairOracle_USDC_WETH',
-  'UniswapPairOracle_USDT_WETH',
-  'UniswapPairOracle_ARTH_ARTHX',
-  'UniswapPairOracle_ARTHX_WETH',
-  'UniswapPairOracle_ARTHX_USDC',
-  'UniswapPairOracle_ARTHX_USDT',
-  'UniswapPairOracle_ARTH_WETH',
-];
-
 async function main() {
   console.log('network', network.name);
 
   // Fetch already deployed contracts.
   const deployements = require(`../output/${network.name}.json`);
+  const instance = await ethers.getContractAt(
+    'ArthController',
+    deployements.ArthController.address
+  );
 
-  for (let i = 0; i < oracles.length; i++) {
-    console.log('updating oracle', i, deployements[oracles[i]].address);
-    const instance = await ethers.getContractAt(
-      'UniswapPairOracle',
-      deployements[oracles[i]].address
-    );
-
-    if (await instance.canUpdate()) await instance.update();
-  }
+  // await instance.setRedeemCollateralRatio(80 * 10000);
+  // await instance.setMintCollateralRatio(90 * 10000);
+  await instance.setGlobalCollateralRatio(100 * 10000);
+  // await instance.toggleUseGlobalCRForRecollateralize(false);
+  await instance.toggleUseGlobalCRForRedeem(true);
+  // await instance.toggleUseGlobalCRForMint(false);
 }
 
 main()
