c667cec45ccb83e0206c2fca3bb640deb9716257
Yash Agrawal <yashagrawal18@outlook.com>
2021-05-01 16:11:55 +0530

    Update params in unit tests

--- test/ARTHController.test.ts
+++ test/ARTHController.test.ts
@@ -25,12 +25,14 @@ describe('ARTHController', () => {
   let timelock: SignerWithAddress;
   let attacker: SignerWithAddress;
 
+  let ARTH: ContractFactory;
   let SimpleOracle: ContractFactory;
   let ARTHController: ContractFactory;
   let MockUniswapOracle: ContractFactory;
   let ChainlinkETHGMUOracle: ContractFactory;
   let MockChainlinkAggregatorV3: ContractFactory;
 
+  let arth: Contract;
   let gmuOracle: Contract;
   let arthController: Contract;
   let arthETHUniswapOracle: Contract;
@@ -43,6 +45,7 @@ describe('ARTHController', () => {
   });
 
   before(' - Fetch contract factories', async () => {
+    ARTH = await ethers.getContractFactory('ARTHStablecoin');
     SimpleOracle = await ethers.getContractFactory('SimpleOracle');
     ARTHController = await ethers.getContractFactory('ArthController');
     MockUniswapOracle = await ethers.getContractFactory('MockUniswapPairOracle');
@@ -51,6 +54,7 @@ describe('ARTHController', () => {
   });
 
   beforeEach(' - Deploy contracts', async () => {
+    arth = await ARTH.deploy();
     gmuOracle = await SimpleOracle.deploy('GMU/USD', ETH);
     arthETHUniswapOracle = await MockUniswapOracle.deploy();
     arthxETHUniswapOracle = await MockUniswapOracle.deploy();
@@ -61,6 +65,7 @@ describe('ARTHController', () => {
       gmuOracle.address
     );
     arthController = await ARTHController.deploy(
+      arth.address,
       owner.address,
       timelock.address
     );
