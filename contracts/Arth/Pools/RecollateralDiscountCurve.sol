d0f4ff7915fe98b959d99549b87ce52e33444f03
Yash Agrawal <yashagrawal18@outlook.com>
2021-05-12 17:21:31 +0530

    Change recollateralize curve and precision for it

--- contracts/Arth/Pools/RecollateralDiscountCurve.sol
+++ contracts/Arth/Pools/RecollateralDiscountCurve.sol
@@ -2,19 +2,21 @@
 
 pragma solidity ^0.8.0;
 
-contract RecollateralDiscountCurve {
-    function getY(uint256 percent) public pure returns (uint256) {
+import {ICurve} from '../../Curves/ICurve.sol';
+
+contract RecollateralDiscountCurve is ICurve {
+    function getY(uint256 percent) external pure override returns (uint256) {
         if (percent <= 0) return 33 ether;
-        else if (percent > 0 && percent <= 10) return 30 ether;
-        else if (percent > 10 && percent <= 20) return 27 ether;
-        else if (percent > 20 && percent <= 30) return 24 ether;
-        else if (percent > 30 && percent <= 40) return 21 ether;
-        else if (percent > 40 && percent <= 50) return 18 ether;
-        else if (percent > 50 && percent <= 60) return 15 ether;
-        else if (percent > 60 && percent <= 70) return 12 ether;
-        else if (percent > 70 && percent <= 80) return 9 ether;
-        else if (percent > 80 && percent <= 90) return 6 ether;
-        else if (percent > 90 && percent <= 100) return 3;
-        else return 0;
+        else if (percent > 0 && percent <= 10e16) return 30; // In %.
+        else if (percent > 10e16 && percent <= 20e16) return 27; // In %.
+        else if (percent > 20e16 && percent <= 30e16) return 24; // In %.
+        else if (percent > 30e16 && percent <= 40e16) return 21; // In %.
+        else if (percent > 40e16 && percent <= 50e16) return 18; // In %.
+        else if (percent > 50e16 && percent <= 60e16) return 15; // In %.
+        else if (percent > 60e16 && percent <= 70e16) return 12; // In %.
+        else if (percent > 70e16 && percent <= 80e16) return 9; // In %.
+        else if (percent > 80e16 && percent <= 90e16) return 6; // In %.
+        else if (percent > 90e16 && percent <= 100e16) return 3; // In %.
+        else return 0; // In %.
     }
 }
