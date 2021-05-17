979600c17efb15b08b94823efa8a0266862a5cc9
Yash Agrawal <yashagrawal18@outlook.com>
2021-05-17 19:04:10 +0530

    Update oracles

--- migrations/16_deploy_genesis.js
+++ migrations/16_deploy_genesis.js
@@ -4,34 +4,39 @@ const BigNumber = require('bignumber.js');
 const helpers = require('./helpers')
 
 
-const Genesis = artifacts.require("Genesis");
-const ARTHShares = artifacts.require("ARTHShares");
+// const Genesis = artifacts.require("Genesis");
+// const ARTHShares = artifacts.require("ARTHShares");
 const BondingCurve = artifacts.require('BondingCurve');
-const ARTHStablecoin = artifacts.require("Arth/ARTHStablecoin");
+const ArthController = artifacts.require('ArthController');
+// const ARTHStablecoin = artifacts.require("Arth/ARTHStablecoin");
 
 
 module.exports = async function (deployer, network) {
-  const arthInstance = await ARTHStablecoin.deployed();
-  const arthxInstance = await ARTHShares.deployed();
+  // const arthInstance = await ARTHStablecoin.deployed();
+  // const arthxInstance = await ARTHShares.deployed();
+  const arthControllerInstance = await ArthController.deployed();
   const bondingCurveInstance = await BondingCurve.deployed();
-  const wethInstance = await helpers.getWETH(network, deployer, artifacts);
-  const mahaInstance = await helpers.getMahaToken(network, deployer, artifacts);
-  const routerInstance = await helpers.getUniswapRouter(network, deployer, artifacts);
-  const ethGmuOracleInstance = await helpers.getChainlinkETHUSDOracle(network, deployer, artifacts);
+  // const wethInstance = await helpers.getWETH(network, deployer, artifacts);
+  // const mahaInstance = await helpers.getMahaToken(network, deployer, artifacts);
+  // const routerInstance = await helpers.getUniswapRouter(network, deployer, artifacts);
+  // const ethGmuOracleInstance = await helpers.getChainlinkETHUSDOracle(network, deployer, artifacts);
 
-  console.log('Deploying Genesis...')
-  await deployer.deploy(
-    Genesis,
-    wethInstance.address,
-    arthInstance.address,
-    arthxInstance.address,
-    mahaInstance.address,
-    routerInstance.address,
-    ethGmuOracleInstance.address,
-    bondingCurveInstance.address,
-    new BigNumber('100e18'), // Soft cap.
-    new BigNumber('200e18'), // Hard cap.
-    Math.floor(Date.now() / 1000), // Start time.
-    24 * 60 * 60 // Duration.
-  );
+  console.log('Linking genesis oracle...')
+  await arthControllerInstance.setBondingCurve(bondingCurveInstance.address);
+
+  // console.log('Deploying Genesis...')
+  // await deployer.deploy(
+  //   Genesis,
+  //   wethInstance.address,
+  //   arthInstance.address,
+  //   arthxInstance.address,
+  //   mahaInstance.address,
+  //   routerInstance.address,
+  //   ethGmuOracleInstance.address,
+  //   bondingCurveInstance.address,
+  //   new BigNumber('100e18'), // Soft cap.
+  //   new BigNumber('200e18'), // Hard cap.
+  //   Math.floor(Date.now() / 1000), // Start time.
+  //   24 * 60 * 60 // Duration.
+  // );
 }
