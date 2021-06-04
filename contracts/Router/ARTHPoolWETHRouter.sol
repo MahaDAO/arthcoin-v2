144d822cb6fe1713c6386dd97b4019f7f1ed9bc2
Yash Agrawal <yashagrawal18@outlook.com>
2021-06-04 23:19:42 +0530

    Remove unncessary imports

--- contracts/Router/ARTHPoolWETHRouter.sol
+++ contracts/Router/ARTHPoolWETHRouter.sol
@@ -5,30 +5,23 @@ pragma solidity ^0.8.0;
 import {IARTH} from '../Arth/IARTH.sol';
 import {IARTHPool} from '../Arth/Pools/IARTHPool.sol';
 import {IARTHX} from '../ARTHX/IARTHX.sol';
-import {IERC20} from '../ERC20/IERC20.sol';
 import {IWETH} from '../ERC20/IWETH.sol';
-import {ISimpleOracle} from '../Oracle/ISimpleOracle.sol';
-import {IBoostedStaking} from '../Staking/IBoostedStaking.sol';
-import {IUniswapV2Router02} from '../Uniswap/Interfaces/IUniswapV2Router02.sol';
 
 contract ARTHPoolWETHRouter {
     IARTH public arth;
     IWETH public weth;
     IARTHX public arthx;
     IARTHPool public wethPool;
-    IUniswapV2Router02 public router;
 
     constructor(
         IWETH weth_,
         IARTH arth_,
         IARTHX arthx_,
-        IARTHPool wethPool_,
-        IUniswapV2Router02 router_
+        IARTHPool wethPool_
     ) {
         arth = arth_;
         weth = weth_;
         arthx = arthx_;
-        router = router_;
         wethPool = wethPool_;
     }
 
