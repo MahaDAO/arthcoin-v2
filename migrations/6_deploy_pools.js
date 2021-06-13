7436a074f5fe6b17be01784f787c719f3bc8768c
Yash Agrawal <yashagrawal18@outlook.com>
2021-06-13 13:53:08 +0530

    Fix more migrations

--- migrations/6_deploy_pools.js
+++ migrations/6_deploy_pools.js
@@ -147,4 +147,10 @@ module.exports = async function (deployer, network, accounts) {
   console.log(chalk.yellow('\nSetting Lottery Contracts in genesis...'))
   await usdtGenesis.setLotteryContract(Lottery.address);
   await usdcGenesis.setLotteryContract(Lottery.address);
+
+  console.log(chalk.yellow('\nLinking Collateral oracles...'));
+  await Promise.all([
+    usdcGenesis.setCollatGMUOracle(usdc_oracle_instance.address, { from: DEPLOYER_ADDRESS }),
+    usdtGenesis.setCollatGMUOracle(usdt_oracle_instance.address, { from: DEPLOYER_ADDRESS })
+  ]);
 };
