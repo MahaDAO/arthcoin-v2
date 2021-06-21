a0215b39caac0c5dd19c92909f3266e2ec31be95
Steven Enamakel <enamakel@vezures.com>
2021-06-21 08:11:21 +0300

    wrote some more scripts to interact with the protocol

--- scripts/updatePools.ts
+++ scripts/updatePools.ts
@@ -34,23 +34,23 @@ async function main() {
 
   await instance.setRedeemCollateralRatio(0);
   await instance.setStabilityFee(0);
-  await instance.setMintCollateralRatio(90 * 10000);
+  // await instance.setMintCollateralRatio(90 * 10000);
   // console.log((await ARTHGMUOracle.consult(deployements.WETH.address, 1e6)).toString())
   // console.log((await pool.getCollateralPrice()).toString())
   // console.log((await GMUOracle.getPrice()).toString())
   // console.log((await GMUOracle.setPrice(1e6)).toString())
 
-  console.log((await instance.getCRForMint()).toString());
-  console.log((await instance.mintCollateralRatio()).toString());
+  // console.log((await instance.getCRForMint()).toString());
+  // console.log((await instance.mintCollateralRatio()).toString());
   // console.log((await instance.getARTHPrice()).toString())
 
   // 1000000/ 2200000000
-  console.log(await pool.getAvailableExcessCollateralDV());
+  // console.log(await pool.getAvailableExcessCollateralDV());
   // // console.log(await pool.getCollateralGMUBalance())
   // await instance.setGlobalCollateralRatio(0 * 10000);
   // console.log(await pool.getAvailableExcessCollateralDV())
   // // await instance.toggleUseGlobalCRForRecollateralize(false);
-  // await instance.toggleUseGlobalCRForRedeem(false);
+  await instance.toggleUseGlobalCRForRedeem(false);
   // // await instance.toggleUseGlobalCRForMint(false);
 
   // console.log(await pool.getAvailableExcessCollateralDV())
