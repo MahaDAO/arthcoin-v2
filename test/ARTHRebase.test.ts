ce67049285f63bb88ea9e178b07ca0d35cafb721
Yash Agrawal <yashagrawal18@outlook.com>
2021-06-22 03:03:56 +0530

    Minor fixes

--- test/ARTHRebase.test.ts
+++ test/ARTHRebase.test.ts
@@ -15,36 +15,20 @@ describe('ARTHController', () => {
   let ARTH: ContractFactory;
   let arth: Contract;
 
-  const GENESIS_SUPPLY = ETH.mul(22100000);
-  const MAX_UINT256 = ethers.constants.MaxUint256;
-  const INITIAL_AMOUNT_SUPPLY = BigNumber.from('21107858507999546111302861');
-  const TOTAL_FRACTIONS = MAX_UINT256.sub(MAX_UINT256.mod(INITIAL_AMOUNT_SUPPLY));
-
   before(' - Setup accounts & deploy libraries', async () => {
     [owner] = await ethers.getSigners();
   });
 
-  before(' - Fetch contract factories', async () => {
+  before(' - Fetch and deploy contract', async () => {
     ARTH = await ethers.getContractFactory('ARTHStablecoin');
-  });
-
-  beforeEach(' - Deploy contracts', async () => {
     arth = await ARTH.deploy();
   });
 
   describe(' - Rebase', async () => {
-    it(' - Should work properly for 1% rebase', async() => {
+    it('  - Should decrease balance by 50% if rebase 50%', async () => {
       const arthBalanceBefore = await arth.balanceOf(owner.address);
-
-      await arth.rebase(BigNumber.from(
-        `-${GENESIS_SUPPLY.mul(1).div(100).toString()}`
-      ));
-
-      expect(await arth.balanceOf(owner.address))
-        .to
-        .eq(
-          arthBalanceBefore.sub(arthBalanceBefore.mul(1).div(100))
-        );
+      await arth.rebase(1e3);
+      expect(await arth.balanceOf(owner.address)).to.eq(arthBalanceBefore.mul(50).div(100));
     });
   });
 });
