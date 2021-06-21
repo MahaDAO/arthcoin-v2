d10e670cd19926b5c53f7ba07714166bd78c6504
Steven Enamakel <enamakel@vezures.com>
2021-06-22 01:32:31 +0300

    set gas price in hardhat and truffle

--- truffle-config.js
+++ truffle-config.js
@@ -37,7 +37,7 @@ module.exports = {
     },
     matic: {
       network_id: 137,
-      gasPrice: 1100000000,  // 1.1 gwei
+      gasPrice: 5100000000,  // 1.1 gwei
       provider: function () {
         return new HDWalletProvider(
           process.env.METAMASK_WALLET_SECRET,
