8427aa58ae0b3a233c90507d9da9b8b8eee65c41
Yash Agrawal <yashagrawal18@outlook.com>
2021-06-24 19:56:57 +0530

    Remove unncessary variables from contract

--- contracts/ARTHX/ARTHXTaxController.sol
+++ contracts/ARTHX/ARTHXTaxController.sol
@@ -2,18 +2,15 @@
 
 pragma solidity ^0.8.0;
 
-import {IERC20Burnable} from '../ERC20/IERC20Burnable.sol';
 import {Ownable} from '../access/Ownable.sol';
 import {SafeMath} from '../utils/math/SafeMath.sol';
 import {ITaxController} from './ITaxController.sol';
-import {IUniswapV2Factory} from '../Uniswap/Interfaces/IUniswapV2Factory.sol';
-import {IUniswapV2Router02} from '../Uniswap/Interfaces/IUniswapV2Router02.sol';
+import {IERC20Burnable} from '../ERC20/IERC20Burnable.sol';
 
 contract ARTHXTaxController is Ownable, ITaxController {
     using SafeMath for uint256;
 
     IERC20Burnable public immutable arthx;
-    IUniswapV2Router02 public immutable router;
 
     uint256 public taxPercentToBurn = 50e4; // In 6 precision.
     uint256 public taxPercentToRewards = 50e4; // In 6 precision.
@@ -22,9 +19,9 @@ contract ARTHXTaxController is Ownable, ITaxController {
 
     event TaxCharged(address from, address to, uint256 amount);
 
-    constructor(IERC20Burnable token, IUniswapV2Router02 swapRouter) {
+    constructor(IERC20Burnable token, address destinationForRewards) {
         arthx = token;
-        router = swapRouter;
+        rewardsDestination = destinationForRewards;
     }
 
     function setPercentToBurn(uint256 percent) external onlyOwner {
