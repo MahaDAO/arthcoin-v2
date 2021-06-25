f570cf6c077564602a50997e2bee3c33e016f7dd
Sagar Behara <sagar@cryptocontrol.io>
2021-06-25 19:09:45 +0530

    implemented an ARTHX pool

---
+++ contracts/Staking/Variants/StakeARTHXrMaha.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+pragma experimental ABIEncoderV2;
+
+import '../BasicStakingSpecificReward.sol';
+
+contract StakeARTHXARTH is BasicStakingSpecificReward {
+    constructor(
+        address _rewardsDistribution,
+        address _rewardsToken,
+        address _stakingToken,
+        uint256 _rewardsDuration
+    )
+        BasicStakingSpecificReward(
+            _rewardsDistribution,
+            _rewardsToken,
+            _stakingToken,
+            7 days
+        )
+    {}
+}
