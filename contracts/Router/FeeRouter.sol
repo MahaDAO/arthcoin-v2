d0455fdbac96dbabc9045e2635553ff55c2fad88
Yash Agrawal <yashagrawal18@outlook.com>
2021-07-06 21:48:22 +0530

    Minor fixes

--- contracts/Router/FeeRouter.sol
+++ contracts/Router/FeeRouter.sol
@@ -15,57 +15,57 @@ contract FeeRouter is Ownable {
     using SafeMath for uint256;
 
     IERC20 public immutable USDC;
-    IUniswapV2Router02 public immutable ROUTER;
 
     uint256 public totalSwapped = 0;
 
     constructor(
-        IERC20 usdc,
-        IUniswapV2Router02 router
+        IERC20 usdc
     ) {
         USDC = usdc;
-        ROUTER = router;
     }
 
     function swap(
-        address token,
+        IERC20 token,
         uint256 amount,
-        address boardroom
+        IBoardroom boardroom,
+        IUniswapV2Router02 router
     ) external payable onlyOwner returns (uint256) {
         // Swap intput tokens for output tokens.
         uint256 usdcRecievedAfterSwap = _swapForUSDC(
             token,
-            amount
+            amount,
+            router
         );
 
         // Update the counter to keep track of swapped tokens.
         totalSwapped = totalSwapped.add(usdcRecievedAfterSwap);
 
         // Approve the boardroom to use these tokens.
-        USDC.approve(boardroom, usdcRecievedAfterSwap);
+        USDC.approve(address(boardroom), usdcRecievedAfterSwap);
 
         // NOTE: this router needs to be operator of boardroom.
-        IBoardroom(boardroom).allocateSeigniorage(usdcRecievedAfterSwap);
+        boardroom.allocateSeigniorage(usdcRecievedAfterSwap);
 
         return usdcRecievedAfterSwap;
     }
 
     function _swapForUSDC(
-        address token,
-        uint256 amountIn
+        IERC20 token,
+        uint256 amountIn,
+        IUniswapV2Router02 router
     ) internal returns (uint256) {
         // Get the amount from the owner.
-        IERC20(token).transferFrom(msg.sender, address(this), amountIn);
-        IERC20(token).approve(address(ROUTER), amountIn);
+        token.transferFrom(msg.sender, address(this), amountIn);
+        token.approve(address(router), amountIn);
 
         // Setup the input and output tokens.
         address[] memory path = new address[](2);
-        path[0] = token;
+        path[0] = address(token);
         path[1] = address(USDC);
 
         // Find the minimum out tokens exepcted and then swap tokens.
-        uint256[] memory amountsOut = ROUTER.getAmountsOut(amountIn, path);
-        uint256[] memory swapAmountsOut = ROUTER.swapExactTokensForTokens(
+        uint256[] memory amountsOut = router.getAmountsOut(amountIn, path);
+        uint256[] memory swapAmountsOut = router.swapExactTokensForTokens(
             amountIn,
             amountsOut[1],
             path,
